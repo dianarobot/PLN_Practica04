a2f577deb4eb56b6d44615d8dea67ce7a2b4b01f
Diana Rocha Botello <diana.botello.93@gmail.com>
2019-09-10 15:36:43 -0500

    FIX Diccionario para el algoritmo de Page Rank

--- pageRank.py
+++ pageRank.py
@@ -24,25 +24,24 @@ class PageRank():
 					values.append(0)
 					values.append(1)
 					values.append([])
-					if (i+1) < len(token):
-						values[2].append(token[i+1].lower())
-					if (i-1) > -1:
-						values[2].append(token[i-1].lower())
 					self.pageR.update({word.lower():values})
-				else:
-					if (i+1) < len(token):
-						pageRankExist[2].append(token[i+1].lower())
-					if (i-1) > -1:
-						pageRankExist[2].append(token[i-1].lower())
-					self.pageR.update({word.lower():pageRankExist})
-				i+=1
-		print(self.pageR)
+				pageRankExist = self.pageR.get(word.lower(), -1)
+				for w in token:
+					if w != word:
+						pageRankExist[2].append(w.lower())
+				self.pageR.update({word.lower():pageRankExist})
+		#print(self.pageR)
 
 	def updateValorActual(self):
 		for k, v in self.pageR.items():
 			v[0] = v[1]
 			self.pageR.update({k:v})
 		#print(self.pageR)
+
+	def printDiccionarioPageRank(self):
+		for k,v in sorted(self.pageR.items(), key=lambda x: x[1][1]):
+			print(k+" -> Actual: "+str(v[1])+" Anterior: "+str(v[1]))
+
 
 	def doPageRank(self, iteraciones):
 		self.definePageRankStructure()
@@ -53,8 +52,8 @@ class PageRank():
 				PR_L = 0 # se utiliza para guardar el valor de PR(X)/L(X)
 				for backLink in values[2]:
 					backLink_link = self.pageR.get(backLink, -1)
-					print(backLink)
-					print(backLink_link)
+					#print(backLink)
+					#print(backLink_link)
 					if len(backLink_link[2]) > 0:
 						PR_L = backLink_link[0] / len(backLink_link[2])
 					else:
@@ -62,12 +61,11 @@ class PageRank():
 					PR += PR_L
 				values[1] = PR
 				self.pageR.update({key:values})
-			print("----------------ITERACIÓN "+str(x)+"----------------")
-			print(self.pageR)
+			print("----------------ITERACIÓN "+str(x+1)+"----------------")
+			self.printDiccionarioPageRank()
 			print("-------------------------------------------")
-		print(self.pageR)
 
 if __name__ == "__main__":
 	p = PageRank()
 	p.getTokenizeSentence()
-	p.doPageRank(2)
+	p.doPageRank(5)
